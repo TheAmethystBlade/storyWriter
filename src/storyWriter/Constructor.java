cbc946ee54811bd395b8275fceff6daf3bcfd211
TheAmethystBlade <harrison.eaves@gmail.com>
2018-03-06 16:11:48 -0600

    Updated Constructor TODO

--- src/storyWriter/Constructor.java
+++ src/storyWriter/Constructor.java
@@ -3,7 +3,8 @@ package storyWriter;
 public class Constructor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Write tag intake code, write the choice making system, write compiler of the story
+		// Important: The choice making system may require an additional class to search for the tags and assotiated elements. Probobly will be located under the Tag system file tree
 
 	}
 
